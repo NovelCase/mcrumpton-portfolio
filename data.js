cb1154714b444159f4e108485488440c20077135
Marie Crumpton <marie.k.cr@gmail.com>
2020-12-31 17:07:07 -0500

    Project pop ups have text

--- data.js
+++ data.js
@@ -1,70 +1,70 @@
-export default text = {
-	gobARk: {
-		name: 'go.bARk',
-		description:
-			'An Augmented Reality virtual pet game, built using the PERN (PostgreSQL, Express, React (and React Native), and Node.js) stack, and integrated with Viromedia’s ViroReact library. go.bARk comes equipped with three dogs to choose from and four finished dog/user interactions.',
-		linkOne: 'Github',
-		linkOneUrl: 'https://github.com/gh-capstone-team-c/Go.bARk',
-		linkTwo: 'Presentation',
-		linkTwoUrl: 'https://www.youtube.com/watch?v=tJig6T0Ccoc',
-	},
-	promiseHS: {
-		name: 'PromiseHS',
-		description:
-			'A 4-day hackathon-style sliding-ice-puzzle game built using React, Express.js, and Pixi.JS.',
-		linkOne: 'Github',
-		linkOneUrl: 'https://github.com/leslie-meng/promiseHS',
-		linkTwo: 'Live Site',
-		linkTwoUrl: 'https://promise-hs.herokuapp.com/',
-	},
-	chai: {
-		name: 'Chai Noon',
-		description:
-			'A mock e-commerce site selling tea products built with the PERN (PostgreSQL, Express, React, Node) Stack, Mocha, Redux, and Chai.',
-		linkOne: 'Github',
-		linkOneUrl: 'https://github.com/grace-shopper-orgs/chai-noon',
-		linkTwo: 'Live Site',
-		linkTwoUrl: 'https://chai-noon.herokuapp.com/',
-	},
-	plants: {
-		name: 'Plants',
-		description:
-			'Currently curating my own personal oasis. A healthy environment encourages inspiration!',
-	},
-	coffee: {
-		name: 'Coffee',
-		description:
-			'Cold brew or pour-over are my go-to prep styles! Depth first approach, because knowing the process means a better understanding and appreciation of what it takes.',
-	},
-	blueOcean: {
-		name: 'Blue Ocean Strategy',
-		description:
-			'An interesting read about integrative approaches! Good concepts, and definitely something to aspire towards.',
-		linkOne: 'Good Reads',
-		linkOneUrl: 'https://www.goodreads.com/book/show/4898.Blue_Ocean_Strategy?',
-	},
-	bfa: {
-		name: 'Bargaining for Advantage',
-		description:
-			'A personal favorite - this book was the first negotiation book I ever read. My favorite anecdote on integrative solutions is in this book!',
-		linkOne: 'Good Reads',
-		linkOneUrl:
-			'https://www.goodreads.com/book/show/23801.Bargaining_for_Advantage?',
-	},
-	convo: {
-		name: 'A Conversation with Fear',
-		description:
-			'My sister introduced this one to me. Good approaches to handling the YIKES zone and putting thought to what bothers you.',
-		linkOne: 'Good Reads',
-		linkOneUrl:
-			'https://www.goodreads.com/book/show/10858506-a-conversation-with-fear?',
-	},
-	presence: {
-		name: 'Presence',
-		description:
-			"Currently reading- I appreciated the author's TED Talk and I am enjoying the anecdotes in this book as well.",
-		linkOne: 'Good Reads',
-		linkOneUrl:
-			'https://www.goodreads.com/bookhttps://www.goodreads.com/book/show/25066556-presence?',
-	},
+export let text = {
+  gobARk: {
+    name: 'go.bARk',
+    description:
+      'An Augmented Reality virtual pet game, built using the PERN (PostgreSQL, Express, React (and React Native), and Node.js) stack, and integrated with Viromedia’s ViroReact library. go.bARk comes equipped with three dogs to choose from and four finished dog/user interactions.',
+    linkOne: 'Github',
+    linkOneUrl: 'https://github.com/gh-capstone-team-c/Go.bARk',
+    linkTwo: 'Presentation',
+    linkTwoUrl: 'https://www.youtube.com/watch?v=tJig6T0Ccoc',
+  },
+  promiseHS: {
+    name: 'PromiseHS',
+    description:
+      'A 4-day hackathon-style sliding-ice-puzzle game built using React, Express.js, and Pixi.JS.',
+    linkOne: 'Github',
+    linkOneUrl: 'https://github.com/leslie-meng/promiseHS',
+    linkTwo: 'Live Site',
+    linkTwoUrl: 'https://promise-hs.herokuapp.com/',
+  },
+  chai: {
+    name: 'Chai Noon',
+    description:
+      'A mock e-commerce site selling tea products built with the PERN (PostgreSQL, Express, React, Node) Stack, Mocha, Redux, and Chai.',
+    linkOne: 'Github',
+    linkOneUrl: 'https://github.com/grace-shopper-orgs/chai-noon',
+    linkTwo: 'Live Site',
+    linkTwoUrl: 'https://chai-noon.herokuapp.com/',
+  },
+  plants: {
+    name: 'Plants',
+    description:
+      'Currently curating my own personal oasis. A healthy environment encourages inspiration!',
+  },
+  coffee: {
+    name: 'Coffee',
+    description:
+      'Cold brew or pour-over are my go-to prep styles! Depth first approach, because knowing the process means a better understanding and appreciation of what it takes.',
+  },
+  blueOcean: {
+    name: 'Blue Ocean Strategy',
+    description:
+      'An interesting read about integrative approaches! Good concepts, and definitely something to aspire towards.',
+    linkOne: 'Good Reads',
+    linkOneUrl: 'https://www.goodreads.com/book/show/4898.Blue_Ocean_Strategy?',
+  },
+  bfa: {
+    name: 'Bargaining for Advantage',
+    description:
+      'A personal favorite - this book was the first negotiation book I ever read. My favorite anecdote on integrative solutions is in this book!',
+    linkOne: 'Good Reads',
+    linkOneUrl:
+      'https://www.goodreads.com/book/show/23801.Bargaining_for_Advantage?',
+  },
+  convo: {
+    name: 'A Conversation with Fear',
+    description:
+      'My sister introduced this one to me. Good approaches to handling the YIKES zone and putting thought to what bothers you.',
+    linkOne: 'Good Reads',
+    linkOneUrl:
+      'https://www.goodreads.com/book/show/10858506-a-conversation-with-fear?',
+  },
+  presence: {
+    name: 'Presence',
+    description:
+      "Currently reading- I appreciated the author's TED Talk and I am enjoying the anecdotes in this book as well.",
+    linkOne: 'Good Reads',
+    linkOneUrl:
+      'https://www.goodreads.com/bookhttps://www.goodreads.com/book/show/25066556-presence?',
+  },
 };
--- pixi/projectStage.js
+++ pixi/projectStage.js
@@ -3,102 +3,102 @@ const PIXI = require('pixi.js');
 const Project = require('../client/ProjectView');
 
 const app = new PIXI.Application({
-	transparent: false,
-	resizeTo: window,
+  transparent: false,
+  resizeTo: window,
 });
 
 app.renderer.backgroundColor = 0xd8d5bb;
 let pixiDiv = document.getElementById('pixi');
 pixiDiv.appendChild(app.view);
 
-let appWidth = app.renderer.view.width;
-let appHeight = app.renderer.view.height;
+export let appWidth = app.renderer.view.width;
+export let appHeight = app.renderer.view.height;
 
 let left = keyboard('ArrowLeft'),
-	up = keyboard('ArrowUp'),
-	right = keyboard('ArrowRight'),
-	down = keyboard('ArrowDown'),
-	space = keyboard(' ' || 'Spacebar');
+  up = keyboard('ArrowUp'),
+  right = keyboard('ArrowRight'),
+  down = keyboard('ArrowDown'),
+  space = keyboard(' ' || 'Spacebar');
 
 //Left arrow key `press` method
 left.press = () => {
-	if (app.stage.pivot.x >= window.innerWidth) {
-		app.stage.pivot.x -= window.innerWidth;
-	}
+  if (app.stage.pivot.x >= window.innerWidth) {
+    app.stage.pivot.x -= window.innerWidth;
+  }
 };
 //Up
 up.press = () => {};
 //Right
 right.press = () => {
-	if (app.stage.pivot.x <= window.innerWidth * 2) {
-		app.stage.pivot.x += window.innerWidth;
-	}
+  if (app.stage.pivot.x <= window.innerWidth * 2) {
+    app.stage.pivot.x += window.innerWidth;
+  }
 };
 
 function keyboard(value) {
-	let key = {};
-	key.value = value;
-	key.isDown = false;
-	key.isUp = true;
-	key.press = undefined;
-	key.release = undefined;
-	//The `downHandler`
-	key.downHandler = (event) => {
-		if (event.key === key.value) {
-			if (key.isUp && key.press) key.press();
-			key.isDown = true;
-			key.isUp = false;
-			event.preventDefault();
-		}
-	};
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = (event) => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
 
-	//The `upHandler`
-	key.upHandler = (event) => {
-		if (event.key === key.value) {
-			if (key.isDown && key.release) key.release();
-			key.isDown = false;
-			key.isUp = true;
-			event.preventDefault();
-		}
-	};
+  //The `upHandler`
+  key.upHandler = (event) => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
 
-	//Attach event listeners
-	const downListener = key.downHandler.bind(key);
-	const upListener = key.upHandler.bind(key);
+  //Attach event listeners
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
 
-	window.addEventListener('keydown', downListener, false);
-	window.addEventListener('keyup', upListener, false);
+  window.addEventListener('keydown', downListener, false);
+  window.addEventListener('keyup', upListener, false);
 
-	// Detach event listeners
-	key.unsubscribe = () => {
-		window.removeEventListener('keydown', downListener);
-		window.removeEventListener('keyup', upListener);
-	};
+  // Detach event listeners
+  key.unsubscribe = () => {
+    window.removeEventListener('keydown', downListener);
+    window.removeEventListener('keyup', upListener);
+  };
 
-	return key;
+  return key;
 }
 
 /****** Background *******/
 
 let ceiling = new PIXI.Graphics();
 ceiling
-	.beginFill(0xf4f5e7)
-	.drawRect(0, 0, appWidth * 4, appHeight / 20)
-	.endFill();
+  .beginFill(0xf4f5e7)
+  .drawRect(0, 0, appWidth * 4, appHeight / 20)
+  .endFill();
 app.stage.addChild(ceiling);
 
 let trim = new PIXI.Graphics();
 trim
-	.beginFill(0xb39b5f)
-	.drawRect(0, ceiling.height, appWidth * 4, ceiling.height / 3)
-	.endFill();
+  .beginFill(0xb39b5f)
+  .drawRect(0, ceiling.height, appWidth * 4, ceiling.height / 3)
+  .endFill();
 app.stage.addChild(trim);
 
 let floor = new PIXI.Graphics();
 floor
-	.beginFill(0xb39b5f)
-	.drawRect(0, appHeight - appHeight * 0.3, appWidth * 4, appHeight / 2)
-	.endFill();
+  .beginFill(0xb39b5f)
+  .drawRect(0, appHeight - appHeight * 0.3, appWidth * 4, appHeight / 2)
+  .endFill();
 app.stage.addChild(floor);
 
 /****** Welcome room *******/
@@ -112,30 +112,30 @@ app.stage.addChild(windowWeather);
 //welcome view scaling
 let welcomeScale = { windows: 0.65, plants: 0.95, card: 0.85 };
 if (appWidth < 400) {
-	welcomeScale.windows = 0.5;
-	welcomeScale.card = 0.5;
-	welcomeScale.plants = 0.375;
+  welcomeScale.windows = 0.5;
+  welcomeScale.card = 0.5;
+  welcomeScale.plants = 0.375;
 } else if (appWidth < 500) {
-	welcomeScale.card = 0.525;
-	welcomeScale.windows = 0.525;
-	welcomeScale.plants = 0.525;
+  welcomeScale.card = 0.525;
+  welcomeScale.windows = 0.525;
+  welcomeScale.plants = 0.525;
 }
 
 //function to create welcome sprites
 function createWelcomeSprite(x, y, texture, type) {
-	const sprite = new Sprite(texture);
-	app.stage.addChild(sprite);
-	sprite.anchor.set(0.5);
-	sprite.position.x = x;
-	sprite.position.y = y;
-	if (type === 'windows') {
-		sprite.scale.set(welcomeScale.windows);
-	} else if (type === 'plants') {
-		sprite.scale.set(welcomeScale.plants);
-	} else {
-		sprite.scale.set(welcomeScale.card);
-	}
-	return sprite;
+  const sprite = new Sprite(texture);
+  app.stage.addChild(sprite);
+  sprite.anchor.set(0.5);
+  sprite.position.x = x;
+  sprite.position.y = y;
+  if (type === 'windows') {
+    sprite.scale.set(welcomeScale.windows);
+  } else if (type === 'plants') {
+    sprite.scale.set(welcomeScale.plants);
+  } else {
+    sprite.scale.set(welcomeScale.card);
+  }
+  return sprite;
 }
 
 //textures
@@ -149,58 +149,58 @@ const monstera = PIXI.Texture.from('/siteAssets/monstera-shadow.png');
 
 //sprites
 let leftWindowSprite = createWelcomeSprite(
-	appWidth / 12,
-	appHeight / 2.5,
-	leftWindow,
-	'windows'
+  appWidth / 12,
+  appHeight / 2.5,
+  leftWindow,
+  'windows'
 );
 app.stage.addChild(leftWindowSprite);
 
 let backWindowSprite = createWelcomeSprite(
-	appWidth / 3.8,
-	appHeight / 3.65,
-	backWindow,
-	'windows'
+  appWidth / 3.8,
+  appHeight / 3.65,
+  backWindow,
+  'windows'
 );
 app.stage.addChild(backWindowSprite);
 
 let helloCardSprite = createWelcomeSprite(
-	appWidth / 2,
-	appHeight / 1.2,
-	helloCard,
-	'card'
+  appWidth / 2,
+  appHeight / 1.2,
+  helloCard,
+  'card'
 );
 app.stage.addChild(helloCardSprite);
 
 let snakeTwoSprite = createWelcomeSprite(
-	appWidth / 8,
-	appHeight / 2.55,
-	snake2,
-	'plants'
+  appWidth / 8,
+  appHeight / 2.55,
+  snake2,
+  'plants'
 );
 app.stage.addChild(snakeTwoSprite);
 
 let snakeOneSprite = createWelcomeSprite(
-	appWidth / 3.5,
-	appHeight / 2,
-	snake1,
-	'plants'
+  appWidth / 3.5,
+  appHeight / 2,
+  snake1,
+  'plants'
 );
 app.stage.addChild(snakeOneSprite);
 
 let marantaSprite = createWelcomeSprite(
-	appWidth / 2,
-	appHeight / 2.7,
-	maranta,
-	'plants'
+  appWidth / 2,
+  appHeight / 2.7,
+  maranta,
+  'plants'
 );
 app.stage.addChild(marantaSprite);
 
 let monsteraShadowSprite = createWelcomeSprite(
-	appWidth / 7,
-	appHeight / 1.4,
-	monstera,
-	'plants'
+  appWidth / 7,
+  appHeight / 1.4,
+  monstera,
+  'plants'
 );
 app.stage.addChild(monsteraShadowSprite);
 
@@ -209,29 +209,29 @@ app.stage.addChild(monsteraShadowSprite);
 //project view scaling
 let scale = { projects: 0.5, desk: 1 };
 if (appWidth < 400) {
-	scale.projects = 0.25;
-	scale.desk = 0.5;
+  scale.projects = 0.25;
+  scale.desk = 0.5;
 } else if (appWidth < 500) {
-	console.log('inside');
-	scale.projects = 0.3;
-	scale.desk = 0.7;
+  console.log('inside');
+  scale.projects = 0.3;
+  scale.desk = 0.7;
 }
 
 //function to create project sprites
 function createSprite(x, y, texture, type) {
-	const sprite = new Sprite(texture);
-	app.stage.addChild(sprite);
-	sprite.anchor.set(0.5);
-	sprite.position.x = x;
-	sprite.position.y = y;
-	if (type === 'desk') {
-		sprite.scale.set(scale.desk);
-	} else {
-		sprite.scale.set(scale.projects);
-		sprite.interactive = true;
-		sprite.buttonMode = true;
-	}
-	return sprite;
+  const sprite = new Sprite(texture);
+  app.stage.addChild(sprite);
+  sprite.anchor.set(0.5);
+  sprite.position.x = x;
+  sprite.position.y = y;
+  if (type === 'desk') {
+    sprite.scale.set(scale.desk);
+  } else {
+    sprite.scale.set(scale.projects);
+    sprite.interactive = true;
+    sprite.buttonMode = true;
+  }
+  return sprite;
 }
 
 /****** Project room *******/
@@ -242,40 +242,54 @@ const barkTexture = PIXI.Texture.from('/siteAssets/gobARk.png');
 const promiseTexture = PIXI.Texture.from('siteAssets/promiseHS.png');
 
 let desk = createSprite(
-	(appWidth / 2) * 3,
-	(appHeight / 4) * 2.6,
-	deskTexture,
-	'desk'
+  (appWidth / 2) * 3,
+  (appHeight / 4) * 2.6,
+  deskTexture,
+  'desk'
 );
 
-let chai = createSprite(
-	(appWidth / 5) * 6,
-	appHeight / 3,
-	chaiTexture,
-	'project'
+export let chai = createSprite(
+  (appWidth / 5) * 6,
+  appHeight / 3,
+  chaiTexture,
+  'project'
 );
 chai.on('mouseover', () => (chai.tint = 0x007ec7));
 chai.on('mouseout', () => (chai.tint = 0xffffff));
+chai.on('click', () => {
+  Project.onClick('project', 'chai', 'promiseHS', 'gobARk');
+  gobARk.interactive = false;
+  promiseHS.interactive = false;
+});
 
-let gobARk = createSprite(
-	(appWidth / 4) * 6,
-	appHeight / 4,
-	barkTexture,
-	'project'
+export let gobARk = createSprite(
+  (appWidth / 4) * 6,
+  appHeight / 4,
+  barkTexture,
+  'project'
 );
 gobARk.on('mouseover', () => (gobARk.tint = 0x007ec7));
 gobARk.on('mouseout', () => (gobARk.tint = 0xffffff));
+gobARk.on('click', () => {
+  Project.onClick('project', 'gobARk', 'promiseHS', 'chai');
+  promiseHS.interactive = false;
+  chai.interactive = false;
+});
 
-let promiseHS = createSprite(
-	(appWidth / 4) * 7.2,
-	appHeight / 3,
-	promiseTexture,
-	'project'
+export let promiseHS = createSprite(
+  (appWidth / 4) * 7.2,
+  appHeight / 3,
+  promiseTexture,
+  'project'
 );
 
 promiseHS.on('mouseover', () => (promiseHS.tint = 0x007ec7));
 promiseHS.on('mouseout', () => (promiseHS.tint = 0xffffff));
-promiseHS.on('click', () => Project.onClick('promise'));
+promiseHS.on('click', () => {
+  Project.onClick('project', 'promise', 'gobARk', 'chai');
+  gobARk.interactive = false;
+  chai.interactive = false;
+});
 
 /****** About Me room *******/
 
@@ -298,6 +312,9 @@ let bfaBook = new PIXI.Sprite(bfaText);
 app.stage.addChild(bfaBook);
 bfaBook.position.x = (window.innerWidth / 4) * 9.25;
 bfaBook.position.y = window.innerHeight / 3;
+bfaBook.interactive = true;
+bfaBook.buttonMode = true;
+bfaBook.on('click', () => Project.onClick('about', 'bfa'));
 
 // let krimTexture = PIXI.Texture.from('/siteAssets/krimson-queen.png');
 // let krimson = new PIXI.Sprite(krimTexture);
@@ -341,15 +358,19 @@ app.stage.addChild(sideboard);
 sideboard.position.x = (window.innerWidth / 2) * 5 - 285;
 sideboard.position.y = window.innerHeight / 2;
 
-let socialsText = PIXI.Texture.from('/siteAssets/socials-small-keys.png');
+let socialsText = PIXI.Texture.from('/siteAssets/socials-board-nk.png');
 let chalkboard = new PIXI.Sprite(socialsText);
 app.stage.addChild(chalkboard);
 chalkboard.position.x = (window.innerWidth / 2) * 7 - 300;
 
+/* Pop Ups */
 export let popUps = new PIXI.Container();
 app.stage.addChild(popUps);
+
+export let text = new PIXI.Container();
+app.stage.addChild(text);
 
 // console.log(Project);
 // console.log(promiseHS.on);
 // console.log(promiseHS);
-// console.log(app);
+console.log(app);
