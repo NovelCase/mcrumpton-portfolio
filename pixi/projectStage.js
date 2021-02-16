1c2d0942a3afacfeed3ac6da87f3003407d86fbb
Leslie Meng <mengli26@gmail.com>
2021-02-15 20:01:24 -0500

    feat: projects & menu

--- pixi/projectStage.js
+++ pixi/projectStage.js
@@ -3,24 +3,24 @@ const PIXI = require('pixi.js');
 var _ = require('lodash');
 
 window.WebFontConfig = {
-  google: {
-    families: ['Nunito Sans'],
-  },
+	google: {
+		families: ['Nunito Sans'],
+	},
 };
 (function () {
-  const wf = document.createElement('script');
-  wf.src = `${
-    document.location.protocol === 'https:' ? 'https' : 'http'
-  }://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  const s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
+	const wf = document.createElement('script');
+	wf.src = `${
+		document.location.protocol === 'https:' ? 'https' : 'http'
+	}://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	const s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
 })();
 
 export const app = new PIXI.Application({
-  transparent: false,
-  resizeTo: window,
+	transparent: false,
+	resizeTo: window,
 });
 
 app.renderer.backgroundColor = 0x1b1c2b;
@@ -34,137 +34,140 @@ const thirdView = secondView * 2;
 const fourthView = secondView * 3;
 
 let left = keyboard('ArrowLeft'),
-  up = keyboard('ArrowUp'),
-  right = keyboard('ArrowRight'),
-  down = keyboard('ArrowDown'),
-  space = keyboard(' ' || 'Spacebar');
+	up = keyboard('ArrowUp'),
+	right = keyboard('ArrowRight'),
+	down = keyboard('ArrowDown'),
+	space = keyboard(' ' || 'Spacebar');
 
 //Left arrow key `press` method
 up.press = () => {
-  if (app.stage.pivot.y >= app.renderer.view.height) {
-    app.stage.pivot.y =
-      Math.floor(app.stage.pivot.y / app.renderer.view.height) *
-        app.renderer.view.height -
-      app.renderer.view.height;
-    console.log(app.stage.pivot.y);
-    if (app.stage.pivot.y === firstView) {
-      app.renderer.backgroundColor = 0x1b1c2b;
-    } else if (app.stage.pivot.y === secondView) {
-      app.renderer.backgroundColor = 0x15112d;
-    } else if (app.stage.pivot.y === thirdView) {
-      app.renderer.backgroundColor = 0x7598aa /* 0xac9caa */ /* 0xeca83f */; //make this color something from lanterns
-    } else if (app.stage.pivot.y === fourthView) {
-      app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
-    }
-  } else app.stage.pivot.y = 0;
+	if (app.stage.pivot.y >= app.renderer.view.height) {
+		app.stage.pivot.y =
+			Math.floor(app.stage.pivot.y / app.renderer.view.height) *
+				app.renderer.view.height -
+			app.renderer.view.height;
+
+		if (app.stage.pivot.y === firstView) {
+			app.renderer.backgroundColor = 0x1b1c2b;
+		} else if (app.stage.pivot.y === secondView) {
+			app.renderer.backgroundColor = 0x15112d;
+		} else if (app.stage.pivot.y === thirdView) {
+			app.renderer.backgroundColor = 0x7598aa /* 0xac9caa */ /* 0xeca83f */; //make this color something from lanterns
+		} else if (app.stage.pivot.y === fourthView) {
+			app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
+		}
+	} else app.stage.pivot.y = 0;
+	menuContainer.position.y = app.stage.pivot.y + 10;
 };
 
 //Right
 down.press = () => {
-  if (app.stage.pivot.y <= app.renderer.view.height * 2) {
-    app.stage.pivot.y =
-      Math.floor(app.stage.pivot.y / app.renderer.view.height) *
-        app.renderer.view.height +
-      app.renderer.view.height;
-    console.log(app.stage.pivot.y);
-    if (app.stage.pivot.y === secondView) {
-      app.renderer.backgroundColor = 0x15112d;
-    } else if (app.stage.pivot.y === thirdView) {
-      app.renderer.backgroundColor = 0x7598aa /* 0xac9caa */ /* 0xeca83f */; //make this color something from lanterns
-    } else if (app.stage.pivot.y === fourthView) {
-      app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
-    }
-  } else app.stage.pivot.y = 3 * app.renderer.view.height;
+	if (app.stage.pivot.y <= app.renderer.view.height * 2) {
+		app.stage.pivot.y =
+			Math.floor(app.stage.pivot.y / app.renderer.view.height) *
+				app.renderer.view.height +
+			app.renderer.view.height;
+		console.log(app.stage.pivot.y);
+		if (app.stage.pivot.y === secondView) {
+			app.renderer.backgroundColor = 0x15112d;
+		} else if (app.stage.pivot.y === thirdView) {
+			app.renderer.backgroundColor = 0x7598aa /* 0xac9caa */ /* 0xeca83f */; //make this color something from lanterns
+		} else if (app.stage.pivot.y === fourthView) {
+			app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
+		}
+	} else app.stage.pivot.y = 3 * app.renderer.view.height;
+	menuContainer.position.y = app.stage.pivot.y + 10;
 };
 
 function keyboard(value) {
-  let key = {};
-  key.value = value;
-  key.isDown = false;
-  key.isUp = true;
-  key.press = undefined;
-  key.release = undefined;
-  //The `downHandler`
-  key.downHandler = (event) => {
-    if (event.key === key.value) {
-      if (key.isUp && key.press) key.press();
-      key.isDown = true;
-      key.isUp = false;
-      event.preventDefault();
-    }
-  };
+	let key = {};
+	key.value = value;
+	key.isDown = false;
+	key.isUp = true;
+	key.press = undefined;
+	key.release = undefined;
+	//The `downHandler`
+	key.downHandler = (event) => {
+		if (event.key === key.value) {
+			if (key.isUp && key.press) key.press();
+			key.isDown = true;
+			key.isUp = false;
+			event.preventDefault();
+		}
+	};
 
-  //The `upHandler`
-  key.upHandler = (event) => {
-    if (event.key === key.value) {
-      if (key.isDown && key.release) key.release();
-      key.isDown = false;
-      key.isUp = true;
-      event.preventDefault();
-    }
-  };
+	//The `upHandler`
+	key.upHandler = (event) => {
+		if (event.key === key.value) {
+			if (key.isDown && key.release) key.release();
+			key.isDown = false;
+			key.isUp = true;
+			event.preventDefault();
+		}
+	};
 
-  onwheel = (event) => {
-    if (
-      app.stage.pivot.y < 0 ||
-      app.stage.pivot.y + (event.deltaY * 1.3 || event.deltaX * 1.3) < 0
-    ) {
-      app.stage.pivot.y = 0;
-      // if (app.stage.pivot.y === firstView) {
-      //   app.renderer.backgroundColor = 0x1b1c2b;
-      // } else if (app.stage.pivot.y === secondView) {
-      //   app.renderer.backgroundColor = 0x15112d;
-      // } else if (app.stage.pivot.y === thirdView) {
-      //   app.renderer.backgroundColor = 0xac9caa /* 0xeca83f */; //make this color something from lanterns
-      // } else if (app.stage.pivot.y === fourthView) {
-      //   app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
-      // }
-    } else if (
-      app.stage.pivot.y > app.renderer.view.height * 3 ||
-      app.stage.pivot.y + (event.deltaY * 1.3 || event.deltaX * 1.3) >
-        app.renderer.view.height * 3
-    ) {
-      app.stage.pivot.y = app.renderer.view.height * 3;
-      // if (app.stage.pivot.y === secondView) {
-      //   app.renderer.backgroundColor = 0x15112d;
-      // } else if (app.stage.pivot.y === thirdView) {
-      //   app.renderer.backgroundColor = 0xac9caa /* 0xeca83f */; //make this color something from lanterns
-      // } else if (app.stage.pivot.y === fourthView) {
-      //   app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
-      // }
-    } else {
-      app.stage.pivot.y += event.deltaY * 1.3 || event.deltaX * 1.3;
-      // if (app.stage.pivot.y === firstView) {
-      //   app.renderer.backgroundColor = 0x1b1c2b;
-      // } else if (app.stage.pivot.y === secondView) {
-      //   app.renderer.backgroundColor = 0x15112d;
-      // } else if (app.stage.pivot.y === thirdView) {
-      //   app.renderer.backgroundColor = 0xac9caa /* 0xeca83f */; //make this color something from lanterns
-      // } else if (app.stage.pivot.y === fourthView) {
-      //   app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
-      // }
-    }
-  };
-  function onWindowResize() {
-    app.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+	onwheel = (event) => {
+		if (
+			app.stage.pivot.y < 0 ||
+			app.stage.pivot.y + (event.deltaY * 1.3 || event.deltaX * 1.3) < 0
+		) {
+			app.stage.pivot.y = 0;
+			// if (app.stage.pivot.y === firstView) {
+			//   app.renderer.backgroundColor = 0x1b1c2b;
+			// } else if (app.stage.pivot.y === secondView) {
+			//   app.renderer.backgroundColor = 0x15112d;
+			// } else if (app.stage.pivot.y === thirdView) {
+			//   app.renderer.backgroundColor = 0xac9caa /* 0xeca83f */; //make this color something from lanterns
+			// } else if (app.stage.pivot.y === fourthView) {
+			//   app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
+			// }
+		} else if (
+			app.stage.pivot.y > app.renderer.view.height * 3 ||
+			app.stage.pivot.y + (event.deltaY * 1.3 || event.deltaX * 1.3) >
+				app.renderer.view.height * 3
+		) {
+			app.stage.pivot.y = app.renderer.view.height * 3;
+			// if (app.stage.pivot.y === secondView) {
+			//   app.renderer.backgroundColor = 0x15112d;
+			// } else if (app.stage.pivot.y === thirdView) {
+			//   app.renderer.backgroundColor = 0xac9caa /* 0xeca83f */; //make this color something from lanterns
+			// } else if (app.stage.pivot.y === fourthView) {
+			//   app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
+			// }
+		} else {
+			app.stage.pivot.y += event.deltaY * 1.3 || event.deltaX * 1.3;
+			// if (app.stage.pivot.y === firstView) {
+			//   app.renderer.backgroundColor = 0x1b1c2b;
+			// } else if (app.stage.pivot.y === secondView) {
+			//   app.renderer.backgroundColor = 0x15112d;
+			// } else if (app.stage.pivot.y === thirdView) {
+			//   app.renderer.backgroundColor = 0xac9caa /* 0xeca83f */; //make this color something from lanterns
+			// } else if (app.stage.pivot.y === fourthView) {
+			//   app.renderer.backgroundColor = 0xe1b058 /* 0xffdd68 */; //make this color something from lanterns
+			// }
+		}
+		menuContainer.position.y = app.stage.pivot.y + 10;
+	};
+	function onWindowResize() {
+		app.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
 
-  //Attach event listeners
-  const downListener = key.downHandler.bind(key);
-  const upListener = key.upHandler.bind(key);
+	//Attach event listeners
+	const downListener = key.downHandler.bind(key);
+	const upListener = key.upHandler.bind(key);
 
-  window.addEventListener('keydown', downListener, false);
-  window.addEventListener('keyup', upListener, false);
-  window.addEventListener('wheel', _.throttle(onwheel, 1000), false);
+	window.addEventListener('keydown', downListener, false);
+	window.addEventListener('keyup', upListener, false);
+	window.addEventListener('wheel', _.throttle(onwheel, 0), false);
 
-  // Detach event listeners
-  key.unsubscribe = () => {
-    window.removeEventListener('keydown', downListener);
-    window.removeEventListener('keyup', upListener);
-    window.removeEventListener('wheel', _.throttle(onwheel, 1000), false);
-  };
+	// Detach event listeners
+	key.unsubscribe = () => {
+		window.removeEventListener('keydown', downListener);
+		window.removeEventListener('keyup', upListener);
+		window.removeEventListener('wheel', _.throttle(onwheel, 0), false);
+	};
 
-  return key;
+	return key;
 }
 
 /* Containers */
@@ -182,3 +185,9 @@ megaContainer.addChild(projectView);
 
 export const finalView = new PIXI.Container();
 megaContainer.addChild(finalView);
+
+export const menuContainer = new PIXI.Container();
+megaContainer.addChild(menuContainer);
+
+menuContainer.position.x = 10;
+menuContainer.position.y = 10;
