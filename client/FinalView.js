e313f2f5d87d57d8b3b14aff3cb63241469860fa
Marie Crumpton <marie.k.cr@gmail.com>
2021-02-21 19:19:29 -0500

    better background and project view

--- client/FinalView.js
+++ client/FinalView.js
@@ -2,199 +2,204 @@ import React from 'react';
 import * as PixiApp from '../pixi/projectStage.js';
 import * as PIXI from 'pixi.js';
 import { data } from '../data';
+// import loading from 'public/siteAssets/finalView/loading.png';
 
 export const onClick = () => {};
 
 let scales = {
-	hobbes: 0.5,
-	floor: 1.2,
-	window: 1.1,
-	spotify: 1.1,
-	bookcase: 1.02,
-	github: 1.1,
-	linkedIn: 1.1,
-	table: 1.1,
-	guestbook: 1.1,
-	resume: 0.46,
+  hobbes: 0.5,
+  floor: 1.2,
+  window: 1.1,
+  spotify: 1.1,
+  bookcase: 1.02,
+  github: 1.1,
+  linkedIn: 1.1,
+  table: 1.1,
+  guestbook: 1.1,
+  resume: 0.9,
 };
 
 let width = PixiApp.app.renderer.view.width;
 let height = PixiApp.app.renderer.view.height;
 
 export default class FinalView extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			visible: false,
-		};
-		this.onClickTap = this.onClickTap.bind(this);
-		this.createSprite.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      visible: false,
+    };
+    this.onClickTap = this.onClickTap.bind(this);
+    this.createSprite.bind(this);
+  }
 
-	onClickTap() {
-		if (this.state.visible) {
-			this.setState({ visible: false });
-			PixiApp.app.renderer.view.width += width / 4;
-		} else {
-			this.setState({ visible: true });
-			PixiApp.app.renderer.view.width -= width / 4;
-		}
-	}
-	createSprite(x, y, texture, type, interactive) {
-		const sprite = new PIXI.Sprite(texture);
-		PixiApp.finalView.addChild(sprite);
-		sprite.anchor.set(0.5);
-		sprite.position.x = x;
-		sprite.position.y = y;
-		sprite.scale.set(scales[type]);
-		if (type === 'floor') {
-			sprite.width = window.innerWidth;
-		} else if (interactive) {
-			sprite.interactive = true;
-			sprite.buttonMode = true;
-		}
-		return sprite;
-	}
+  onClickTap() {
+    if (this.state.visible) {
+      this.setState({ visible: false });
+      PixiApp.app.renderer.view.width += width / 4;
+    } else {
+      this.setState({ visible: true });
+      PixiApp.app.renderer.view.width -= width / 4;
+    }
+  }
+  createSprite(x, y, texture, type, interactive) {
+    const sprite = new PIXI.Sprite(texture);
+    PixiApp.finalView.addChild(sprite);
+    sprite.anchor.set(0.5);
+    sprite.position.x = x;
+    sprite.position.y = y;
+    sprite.scale.set(scales[type]);
+    if (type === 'floor') {
+      sprite.width = window.innerWidth;
+    } else if (interactive) {
+      sprite.interactive = true;
+      sprite.buttonMode = true;
+    }
+    return sprite;
+  }
 
-	componentDidMount() {
-		/* textures */
-		const hobbesTexture = PIXI.Texture.from('siteAssets/finalView/Hobbes.png');
-		const bookTexture = PIXI.Texture.from('siteAssets/finalView/bookCase.png');
-		const floorTexture = PIXI.Texture.from('siteAssets/finalView/floor.png');
-		const githubTexture = PIXI.Texture.from('siteAssets/finalView/github.png');
-		const guestbookTexture = PIXI.Texture.from(
-			'siteAssets/finalView/guestbook.png'
-		);
-		const linkedinTexture = PIXI.Texture.from(
-			'siteAssets/finalView/linkedIn.png'
-		);
-		const spotfiyTexture = PIXI.Texture.from(
-			'siteAssets/finalView/spotify.png'
-		);
-		const tableTexture = PIXI.Texture.from('siteAssets/finalView/table.png');
-		const windowTexture = PIXI.Texture.from('siteAssets/finalView/window.png');
-		const resumeTexture = PIXI.Texture.from('siteAssets/finalView/Resume.png');
+  componentDidMount() {
+    /* textures */
+    const hobbesTexture = PIXI.Texture.from('siteAssets/finalView/Hobbes.png');
+    const bookTexture = PIXI.Texture.from(
+      'siteAssets/finalView/bookCaseShadow.png'
+    );
+    const floorTexture = PIXI.Texture.from('siteAssets/finalView/floor.png');
+    const githubTexture = PIXI.Texture.from('siteAssets/finalView/github.png');
+    const guestbookTexture = PIXI.Texture.from(
+      'siteAssets/finalView/guestbook.png'
+    );
+    const linkedinTexture = PIXI.Texture.from(
+      'siteAssets/finalView/linkedIn.png'
+    );
+    const spotfiyTexture = PIXI.Texture.from(
+      'siteAssets/finalView/spotify.png'
+    );
+    const tableTexture = PIXI.Texture.from('siteAssets/finalView/table.png');
+    const windowTexture = PIXI.Texture.from(
+      'siteAssets/finalView/darkWindow.png'
+    );
+    const resumeTexture = PIXI.Texture.from('siteAssets/finalView/Resume.png');
 
-		const floor = this.createSprite(
-			PixiApp.app.renderer.view.width / 2,
-			(PixiApp.app.renderer.view.height / 2) * 6.95,
-			floorTexture,
-			'floor'
-		);
+    const floor = this.createSprite(
+      PixiApp.app.renderer.view.width / 2,
+      (PixiApp.app.renderer.view.height / 2) * 7.07,
+      floorTexture,
+      'floor'
+    );
 
-		const windowSprite = this.createSprite(
-			(PixiApp.app.renderer.view.width / 4) * 2.5,
-			(PixiApp.app.renderer.view.height / 2) * 6.721,
-			windowTexture,
-			'window'
-		);
+    const windowSprite = this.createSprite(
+      (PixiApp.app.renderer.view.width / 4) * 2.7,
+      (PixiApp.app.renderer.view.height / 2) * 6.84,
+      windowTexture,
+      'window'
+    );
 
-		const spotify = this.createSprite(
-			(PixiApp.app.renderer.view.width / 4) * 3.599,
-			(PixiApp.app.renderer.view.height / 2) * 7.05,
-			spotfiyTexture,
-			'spotify',
-			true
-		);
+    const spotify = this.createSprite(
+      (PixiApp.app.renderer.view.width / 4) * 3.599,
+      (PixiApp.app.renderer.view.height / 2) * 7.16,
+      spotfiyTexture,
+      'spotify',
+      true
+    );
 
-		spotify.on('pointertap', () => {
-			this.onClickTap();
-		});
+    spotify.on('pointertap', () => {
+      this.onClickTap();
+    });
 
-		const bookCase = this.createSprite(
-			PixiApp.app.renderer.view.width / 5,
-			(PixiApp.app.renderer.view.height / 2) * 6.748,
-			bookTexture,
-			'bookcase'
-		);
+    const bookCase = this.createSprite(
+      PixiApp.app.renderer.view.width / 5,
+      (PixiApp.app.renderer.view.height / 2) * 6.84,
+      bookTexture,
+      'bookcase'
+    );
 
-		const github = this.createSprite(
-			(PixiApp.app.renderer.view.width / 5) * 1.15,
-			(PixiApp.app.renderer.view.height / 2) * 6.3,
-			githubTexture,
-			'github',
-			true
-		);
+    const github = this.createSprite(
+      (PixiApp.app.renderer.view.width / 5) * 1.16,
+      (PixiApp.app.renderer.view.height / 2) * 6.391,
+      githubTexture,
+      'github',
+      true
+    );
 
-		github.on('pointertap', () => {
-			window.open(data.github, '_blank');
-			PixiApp.app.stage.pivot.y = PixiApp.fourthView;
-		});
+    github.on('pointertap', () => {
+      window.open(data.github, '_blank');
+      PixiApp.app.stage.pivot.y = PixiApp.fourthView;
+    });
 
-		const linkedIn = this.createSprite(
-			(PixiApp.app.renderer.view.width / 5) * 0.78,
-			(PixiApp.app.renderer.view.height / 2) * 6.757,
-			linkedinTexture,
-			'linkedIn',
-			true
-		);
+    const linkedIn = this.createSprite(
+      (PixiApp.app.renderer.view.width / 5) * 0.75,
+      (PixiApp.app.renderer.view.height / 2) * 6.882,
+      linkedinTexture,
+      'linkedIn',
+      true
+    );
 
-		linkedIn.on('pointertap', () => {
-			window.open(data.linkedIn, '_blank');
-			PixiApp.app.stage.pivot.y = PixiApp.fourthView;
-		});
+    linkedIn.on('pointertap', () => {
+      window.open(data.linkedIn, '_blank');
+      PixiApp.app.stage.pivot.y = PixiApp.fourthView;
+    });
 
-		const resume = this.createSprite(
-			(PixiApp.app.renderer.view.width / 5) * 1.33,
-			(PixiApp.app.renderer.view.height / 2) * 6.759,
-			resumeTexture,
-			'resume',
-			true
-		);
+    const resume = this.createSprite(
+      (PixiApp.app.renderer.view.width / 5) * 1.37,
+      (PixiApp.app.renderer.view.height / 2) * 6.88,
+      resumeTexture,
+      'resume',
+      true
+    );
 
-		resume.on('pointertap', () => {
-			window.open(data.resume, '_blank');
-			PixiApp.app.stage.pivot.y = PixiApp.fourthView;
-		});
+    resume.on('pointertap', () => {
+      window.open(data.resume, '_blank');
+      PixiApp.app.stage.pivot.y = PixiApp.fourthView;
+    });
 
-		const table = this.createSprite(
-			(PixiApp.app.renderer.view.width / 4) * 2.5,
-			(PixiApp.app.renderer.view.height / 2) * 7.4,
-			tableTexture,
-			'table'
-		);
+    const table = this.createSprite(
+      (PixiApp.app.renderer.view.width / 4) * 2.55,
+      (PixiApp.app.renderer.view.height / 2) * 7.49,
+      tableTexture,
+      'table'
+    );
 
-		const guestbook = this.createSprite(
-			(PixiApp.app.renderer.view.width / 4) * 2.5,
-			(PixiApp.app.renderer.view.height / 2) * 7,
-			guestbookTexture,
-			'guestbook',
-			true
-		);
+    const guestbook = this.createSprite(
+      (PixiApp.app.renderer.view.width / 4) * 2.52,
+      (PixiApp.app.renderer.view.height / 2) * 7.06,
+      guestbookTexture,
+      'guestbook',
+      true
+    );
 
-		guestbook.on('pointertap', () => {
-			window.location.href =
-				'mailto:marie.k.cr@gmail.com?subject=Just visited your website!';
-			PixiApp.app.stage.pivot.y = PixiApp.fourthView;
-		});
+    guestbook.on('pointertap', () => {
+      window.location.href =
+        'mailto:marie.k.cr@gmail.com?subject=Just visited your website!';
+      PixiApp.app.stage.pivot.y = PixiApp.fourthView;
+    });
 
-		const hobbes = this.createSprite(
-			(PixiApp.app.renderer.view.width / 4) * 1.4,
-			(PixiApp.app.renderer.view.height / 2) * 7.275,
-			hobbesTexture,
-			'hobbes',
-			true
-		);
-	}
-	render() {
-		return (
-			<div>
-				{this.state.visible ? (
-					<div id='container'>
-						<iframe
-							src={data.spotify}
-							width={width / 4}
-							height={height}
-							// frameborder='0'
-							//allowtransparency='true'
-							allow='encrypted-media'
-						></iframe>
-						<img id='loading' src='https://i.ibb.co/TWhb20W/Loading.png' />
-					</div>
-				) : (
-					<div />
-				)}
-			</div>
-		);
-	}
+    const hobbes = this.createSprite(
+      (PixiApp.app.renderer.view.width / 4) * 1.4,
+      (PixiApp.app.renderer.view.height / 2) * 7.38,
+      hobbesTexture,
+      'hobbes',
+      true
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.state.visible ? (
+          <div id="container">
+            <iframe
+              src={data.spotify}
+              width={width / 4}
+              height={height}
+              // frameborder='0'
+              //allowtransparency='true'
+              allow="encrypted-media"
+            ></iframe>
+            {/* <img id="loading" src={loading} /> */}
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  }
 }
--- client/ProjectView.js
+++ client/ProjectView.js
@@ -5,273 +5,262 @@ import { data } from '../data';
 import { Scrollbox } from 'pixi-scrollbox';
 
 export function createPopUpRect(title) {
-	popUpProject.removeChildren();
-	let x = PixiApp.app.renderer.view.width / 4;
-	let y = (PixiApp.app.renderer.view.height * 9) / 4;
-	let width = PixiApp.app.renderer.view.width / 2;
-	let height = PixiApp.app.renderer.view.height / 2;
-	console.log(window, PixiApp.app.renderer.view.width);
-	if (window.outerWidth < 400) {
-		x = 0;
-		y = PixiApp.app.renderer.view.height * 2.125;
-		width = window.outerWidth;
-		height = PixiApp.app.renderer.view.height * 0.75;
-	}
-	const rect = new PIXI.Graphics();
-	rect.beginFill(0xc2b9e1).drawRoundedRect(x, y, width, height, 20).endFill();
-	rect.visible = true;
+  popUpProject.removeChildren();
+  let x = PixiApp.app.renderer.view.width / 4;
+  let y = (PixiApp.app.renderer.view.height * 9) / 4;
+  let width = PixiApp.app.renderer.view.width / 2;
+  let height = PixiApp.app.renderer.view.height / 2;
+  console.log(window, PixiApp.app.renderer.view.width);
+  if (window.outerWidth < 400) {
+    x = 0;
+    y = PixiApp.app.renderer.view.height * 2.125;
+    width = window.outerWidth;
+    height = PixiApp.app.renderer.view.height * 0.75;
+  }
+  const rect = new PIXI.Graphics();
+  rect.beginFill(0xc2b9e1).drawRoundedRect(x, y, width, height, 20).endFill();
+  rect.visible = true;
 
-	const closeButton = new PIXI.Graphics();
+  const closeButton = new PIXI.Graphics();
 
-	closeButton
-		.beginFill(0x000000)
-		.drawCircle(x + 20, y + 15, 10)
-		.endFill();
+  closeButton
+    .beginFill(0x000000)
+    .drawCircle(x + 20, y + 15, 10)
+    .endFill();
 
-	closeButton.visible = true;
-	closeButton.interactive = true;
-	closeButton.buttonMode = true;
-	closeButton.on('pointertap', function () {
-		popUpProject.removeChildren();
-		popUpProject.visible = true;
-	});
-	popUpProject.addChild(rect);
-	popUpProject.addChild(closeButton);
-	let popTitle = createText(
-		data[title].name,
-		titleStyle,
-		x + 50,
-		y + 75,
-		false,
-		'projectTitle'
-	);
-	scrollbox = popUpProject.addChild(
-		new Scrollbox({
-			boxWidth: rect.width - 90,
-			boxHeight: rect.height - 210,
-		})
-	);
-	let projectDetails = scrollbox.content.addChild(new PIXI.Graphics());
-	projectDetails
-		.beginFill(0xc2b9e1, 0.25) /* 0xe3cdfe */
-		.drawRect(0, 0, rect.width - 90, rect.height - 210)
-		.endFill();
-	scrollbox.position.set(x + 50, y + 160);
+  closeButton.visible = true;
+  closeButton.interactive = true;
+  closeButton.buttonMode = true;
+  closeButton.on('pointertap', function () {
+    popUpProject.removeChildren();
+    popUpProject.visible = true;
+  });
+  popUpProject.addChild(rect);
+  popUpProject.addChild(closeButton);
+  let popTitle = createText(
+    data[title].name,
+    titleStyle,
+    x + 50,
+    y + 75,
+    false,
+    'projectTitle'
+  );
+  scrollbox = popUpProject.addChild(
+    new Scrollbox({
+      boxWidth: rect.width - 90,
+      boxHeight: rect.height - 210,
+    })
+  );
+  let projectDetails = scrollbox.content.addChild(new PIXI.Graphics());
+  projectDetails
+    .beginFill(0xc2b9e1, 0.25) /* 0xe3cdfe */
+    .drawRect(0, 0, rect.width - 90, rect.height - 210)
+    .endFill();
+  scrollbox.position.set(x + 50, y + 160);
 
-	let popDesc = createText(
-		data[title].description,
-		descriptionStyle,
-		0,
-		0,
-		false,
-		'projectDescription'
-	);
-	scrollbox.update();
-	let popLinkOne = createText(
-		data[title].linkOne,
-		descriptionStyle,
-		x + 75,
-		y + rect.height - 50,
-		true,
-		'projectGithub'
-	);
-	popLinkOne.on('pointertap', () => openLink(title, 'One'));
+  let popDesc = createText(
+    data[title].description,
+    descriptionStyle,
+    0,
+    0,
+    false,
+    'projectDescription'
+  );
+  scrollbox.update();
+  let popLinkOne = createText(
+    data[title].linkOne,
+    descriptionStyle,
+    x + 75,
+    y + rect.height - 50,
+    true,
+    'projectGithub'
+  );
+  popLinkOne.on('pointertap', () => openLink(title, 'One'));
 
-	let popLinkTwo = createText(
-		data[title].linkTwo,
-		descriptionStyle,
-		x + rect.width - 150,
-		y + rect.height - 50,
-		true,
-		'projectLive'
-	);
-	popLinkTwo.on('pointertap', () => openLink(title, 'Two'));
-	return popUpProject;
+  let popLinkTwo = createText(
+    data[title].linkTwo,
+    descriptionStyle,
+    x + rect.width - 150,
+    y + rect.height - 50,
+    true,
+    'projectLive'
+  );
+  popLinkTwo.on('pointertap', () => openLink(title, 'Two'));
+  return popUpProject;
 }
 
 function openLink(projectName, linkType) {
-	linkType = 'link' + linkType + 'Url';
-	window.open(`${data[projectName][linkType]}`);
+  linkType = 'link' + linkType + 'Url';
+  window.open(`${data[projectName][linkType]}`);
 }
 
 function createText(words, style, x, y, interactive, type) {
-	const smallProject = {
-		projectTitle: {
-			y: (PixiApp.app.renderer.view.height / 2) * 0.4,
-		},
-		projectDescription: {
-			y: (PixiApp.app.renderer.view.height / 2) * 0.55,
-		},
-		projectGithub: {
-			x: (PixiApp.app.renderer.view.width / 2) * 2.2,
-		},
-		projectLive: {
-			x: (PixiApp.app.renderer.view.width / 2) * 2.7,
-		},
-	};
-	const styleTwo = { ...style };
-	if (PixiApp.app.renderer.view.width < 400 && type.includes('project')) {
-		smallProject[type].x
-			? (x = smallProject[type].x)
-			: (x = (PixiApp.app.renderer.view.width / 2) * 2.2);
-		smallProject[type].y
-			? (y = smallProject[type].y)
-			: (y = (PixiApp.app.renderer.view.height / 4) * 2.95);
-		styleTwo.fontSize = styleTwo.fontSize - 5;
-	} else if (PixiApp.app.renderer.view.width < 400) {
-		smallProject[type].x
-			? (x = smallProject[type].x)
-			: (x = (PixiApp.app.renderer.view.width / 2) * 4.2);
-		smallProject[type].y
-			? (y = smallProject[type].y)
-			: (y = (PixiApp.app.renderer.view.height / 4) * 2.95);
-		styleTwo.fontSize = styleTwo.fontSize - 5;
-	}
-	const text = new PIXI.Text(words, styleTwo);
-	text.visible = true;
-	text.position.x = x;
-	text.position.y = y;
-	if (interactive) {
-		text.interactive = true;
-		text.buttonMode = true;
-	}
-	if (type !== 'projectDescription') popUpProject.addChild(text);
-	else scrollbox.content.addChild(text);
-	return text;
+  const smallProject = {
+    projectTitle: {
+      y: (PixiApp.app.renderer.view.height / 2) * 0.4,
+    },
+    projectDescription: {
+      y: (PixiApp.app.renderer.view.height / 2) * 0.55,
+    },
+    projectGithub: {
+      x: (PixiApp.app.renderer.view.width / 2) * 2.2,
+    },
+    projectLive: {
+      x: (PixiApp.app.renderer.view.width / 2) * 2.7,
+    },
+  };
+  const styleTwo = { ...style };
+  if (PixiApp.app.renderer.view.width < 400 && type.includes('project')) {
+    smallProject[type].x
+      ? (x = smallProject[type].x)
+      : (x = (PixiApp.app.renderer.view.width / 2) * 2.2);
+    smallProject[type].y
+      ? (y = smallProject[type].y)
+      : (y = (PixiApp.app.renderer.view.height / 4) * 2.95);
+    styleTwo.fontSize = styleTwo.fontSize - 5;
+  } else if (PixiApp.app.renderer.view.width < 400) {
+    smallProject[type].x
+      ? (x = smallProject[type].x)
+      : (x = (PixiApp.app.renderer.view.width / 2) * 4.2);
+    smallProject[type].y
+      ? (y = smallProject[type].y)
+      : (y = (PixiApp.app.renderer.view.height / 4) * 2.95);
+    styleTwo.fontSize = styleTwo.fontSize - 5;
+  }
+  const text = new PIXI.Text(words, styleTwo);
+  text.visible = true;
+  text.position.x = x;
+  text.position.y = y;
+  if (interactive) {
+    text.interactive = true;
+    text.buttonMode = true;
+  }
+  if (type !== 'projectDescription') popUpProject.addChild(text);
+  else scrollbox.content.addChild(text);
+  return text;
 }
 
 let scrollbox;
 let popUpProject = new PIXI.Container();
 let textInfo = {};
 let scales = {
-	medallionR: 1.3,
-	bluePlant: 0.65,
-	floor: 0.6,
-	deskH: 1.16,
-	deskW: 1.29,
-	gobARk: 0.85,
-	seeTurtleExploration: 0.85,
-	brosApothecary: 0.83,
-	plantsR: 1.2,
-	plantsL: 1.2,
+  floor: 0.6,
+  deskH: 0.8,
+  deskW: 1.29,
+  gobARk: 0.37 /* 0.85 */,
+  seeTurtleExploration: 0.39 /* 0.85 */,
+  brosApothecary: 0.42 /* 0.83 */,
+  plantsR: 1,
+  plantsL: 1,
+  panel: 0.45,
 };
 /* Styling */
 let titleStyle = {
-	fontFamily: 'Montserrat',
-	fontSize: 35,
-	fontWeight: '600',
-	wordWrap: true,
-	wordWrapWidth: PixiApp.app.renderer.view.width / 2 - 150,
+  fontFamily: 'Montserrat',
+  fontSize: 35,
+  fontWeight: '600',
+  wordWrap: true,
+  wordWrapWidth: PixiApp.app.renderer.view.width / 2 - 150,
 };
 let descriptionStyle = {
-	fontFamily: 'Montserrat',
-	fontSize: 23,
-	fontWeight: '400',
-	lineHeight: 50,
-	wordWrap: true,
-	wordWrapWidth: PixiApp.app.renderer.view.width / 2 - 150,
+  fontFamily: 'Montserrat',
+  fontSize: 23,
+  fontWeight: '400',
+  lineHeight: 50,
+  wordWrap: true,
+  wordWrapWidth: PixiApp.app.renderer.view.width / 2 - 150,
 };
 let linkStyle = {
-	fontFamily: 'Montserrat',
-	fontSize: 23,
-	fill: '#007EC7',
+  fontFamily: 'Montserrat',
+  fontSize: 23,
+  fill: '#007EC7',
 };
 export default class ProjectView extends React.Component {
-	createSprite(x, y, texture, type, interactive) {
-		const sprite = new PIXI.Sprite(texture);
-		PixiApp.projectView.addChild(sprite);
-		sprite.anchor.set(0.5);
-		sprite.position.x = x;
-		sprite.position.y = y;
-		sprite.scale.set(scales[type]);
-		if (type === 'floor') {
-			sprite.width = PixiApp.app.renderer.view.width;
-		} else if (type === 'desk') {
-			sprite.scale.x = scales.deskW;
-			sprite.scale.y = scales.deskH;
-		} else if (interactive) {
-			sprite.interactive = true;
-			sprite.buttonMode = true;
-			sprite.on('pointertap', function () {
-				createPopUpRect(type);
-			});
-		}
+  createSprite(x, y, texture, type, interactive) {
+    const sprite = new PIXI.Sprite(texture);
+    PixiApp.projectView.addChild(sprite);
+    sprite.anchor.set(0.5);
+    sprite.position.x = x;
+    sprite.position.y = y;
+    sprite.scale.set(scales[type]);
+    if (interactive) {
+      sprite.interactive = true;
+      sprite.buttonMode = true;
+      sprite.on('pointertap', function () {
+        createPopUpRect(type);
+      });
+    }
 
-		return sprite;
-	}
-	componentDidMount() {
-		/* textures */
+    return sprite;
+  }
+  componentDidMount() {
+    /* textures */
 
-		const floorTexture = PIXI.Texture.from('siteAssets/projectView/floor.png');
-		const deskTexture = PIXI.Texture.from('siteAssets/projectView/desk.png');
-		const plantsRTexture = PIXI.Texture.from(
-			'siteAssets/projectView/plantsRightEdit.png'
-		);
-		const plantsLTexture = PIXI.Texture.from(
-			'siteAssets/projectView/plantsLeftEdit.png'
-		);
-		const barkTexture = PIXI.Texture.from('siteAssets/projectView/gobARk.png');
-		const broTexture = PIXI.Texture.from(
-			'siteAssets/projectView/brosApothecary.png'
-		);
-		const seeTexture = PIXI.Texture.from(
-			'siteAssets/projectView/seeTurtle.png'
-		);
+    const plantsRTexture = PIXI.Texture.from(
+      'siteAssets/projectView/plantsRightEdit.png'
+    );
+    const plantsLTexture = PIXI.Texture.from(
+      'siteAssets/projectView/plantsLeftEdit.png'
+    );
+    const barkTexture = PIXI.Texture.from('siteAssets/projectView/gobARk.png');
+    const broTexture = PIXI.Texture.from(
+      'siteAssets/projectView/brosApothecary.png'
+    );
+    const seeTexture = PIXI.Texture.from(
+      'siteAssets/projectView/seeTurtle.png'
+    );
 
-		const floor = this.createSprite(
-			PixiApp.app.renderer.view.width / 2,
-			(PixiApp.app.renderer.view.height / 2) * 4.928,
-			floorTexture,
-			'floor'
-		);
+    const panelTexture = PIXI.Texture.from(
+      'siteAssets/projectView/animationPanelEdit.png'
+    );
 
-		const desk = this.createSprite(
-			PixiApp.app.renderer.view.width / 2,
-			(PixiApp.app.renderer.view.height / 2) * 4.99,
-			deskTexture,
-			'desk'
-		);
+    const panel = this.createSprite(
+      (PixiApp.app.renderer.view.width / 2) * 0.95,
+      (PixiApp.app.renderer.view.height / 2) * 5.13,
+      panelTexture,
+      'panel'
+    );
 
-		const gobARk = this.createSprite(
-			(PixiApp.app.renderer.view.width / 2) * 0.59,
-			(PixiApp.app.renderer.view.height / 2) * 5.47,
-			barkTexture,
-			'gobARk',
-			true
-		);
+    const gobARk = this.createSprite(
+      (PixiApp.app.renderer.view.width / 2) * 0.78,
+      (PixiApp.app.renderer.view.height / 2) * 5.31,
+      barkTexture,
+      'gobARk',
+      true
+    );
 
-		const seeTurtle = this.createSprite(
-			(PixiApp.app.renderer.view.width / 2) * 1.4,
-			(PixiApp.app.renderer.view.height / 2) * 5.47,
-			seeTexture,
-			'seeTurtleExploration',
-			true
-		);
+    const seeTurtle = this.createSprite(
+      (PixiApp.app.renderer.view.width / 2) * 1.1,
+      (PixiApp.app.renderer.view.height / 2) * 5.31,
+      seeTexture,
+      'seeTurtleExploration',
+      true
+    );
 
-		const brosApothecary = this.createSprite(
-			(PixiApp.app.renderer.view.width / 2) * 1.4,
-			(PixiApp.app.renderer.view.height / 2) * 4.53,
-			broTexture,
-			'brosApothecary',
-			true
-		);
+    const brosApothecary = this.createSprite(
+      (PixiApp.app.renderer.view.width / 2) * 1.102,
+      (PixiApp.app.renderer.view.height / 2) * 4.91,
+      broTexture,
+      'brosApothecary',
+      true
+    );
 
-		const plantsR = this.createSprite(
-			(PixiApp.app.renderer.view.width / 2) * 1.6289,
-			(PixiApp.app.renderer.view.height / 2) * 4.8497,
-			plantsRTexture,
-			'plantsR'
-		);
+    const plantsR = this.createSprite(
+      (PixiApp.app.renderer.view.width / 2) * 1.67,
+      (PixiApp.app.renderer.view.height / 2) * 4.8497,
+      plantsRTexture,
+      'plantsR'
+    );
 
-		const plantsL = this.createSprite(
-			(PixiApp.app.renderer.view.width / 2) * 0.179,
-			(PixiApp.app.renderer.view.height / 2) * 4.8501,
-			plantsLTexture,
-			'plantsL'
-		);
-		PixiApp.projectView.addChild(popUpProject);
-	}
-	render() {
-		return <div></div>;
-	}
+    const plantsL = this.createSprite(
+      (PixiApp.app.renderer.view.width / 2) * 0.14,
+      (PixiApp.app.renderer.view.height / 2) * 4.8501,
+      plantsLTexture,
+      'plantsL'
+    );
+    PixiApp.projectView.addChild(popUpProject);
+  }
+  render() {
+    return <div></div>;
+  }
 }
