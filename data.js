676d39a699a9f4c63f016830f4d409911343ea43
Marie Crumpton <marie.k.cr@gmail.com>
2021-03-05 09:38:21 -0500

    spotify fixed, added scrollbox to lantern view, and added gray background behind popups

--- data.js
+++ data.js
@@ -20,7 +20,7 @@ export let data = {
       'Ecommerce site focused on delivering products, such as facial cleansers and candles, to meet the apothecary needs of everyone.',
     linkOne: 'Github',
     linkOneUrl: 'https://bit.ly/3l4m22J',
-    linkTwo: 'Deployed',
+    linkTwo: 'Live Site',
     linkTwoUrl: 'https://bros-apothecary.herokuapp.com/',
   },
   seeTurtleExploration: {
@@ -33,10 +33,13 @@ export let data = {
     linkTwoUrl: 'https://www.youtube.com/watch?v=dUDTRJXvqfY',
   },
   novelCase: {
-    name: 'novel Case',
-    description: 'Coming soon!',
-    // linkOne: 'Deployed',
-    // linkeOneUrl: '',
+    name: 'Novel Case',
+    description:
+      'Novel Case is the small but driven development team behind this and other websites! Founded by Leslie Meng, Jackie Feit, and me, this team has banked numerous hours dedicated to creating unique solutions for personal experiences.',
+    linkOne: 'Github',
+    linkOneUrl: 'https://github.com/NovelCase',
+    linkTwo: 'Live Site',
+    linkTwoUrl: 'https://github.com/NovelCase',
   },
   stack: {
     name: 'Stack: ',
@@ -50,22 +53,28 @@ export let data = {
 
   spotify: 'https://open.spotify.com/embed/playlist/1ieXH939lQPONeIlh1ILDo',
   aboutMe: {
+    name: 'About Me',
     description:
-      'About Me \n I need to find a way to fill this up with info about me, but I am also concerned about the look of everything.',
+      "Hi! I'm Marie, a full-stack software developer with a background in education. motivated by life long learning, SOMETHING ABOUT CREATIVITY, and striving to find the solution. \n As a recent graduate of the Grace Hopper Program at Fullstack Academy my journey to software development began with one-to-one technology.  ? SOMETHING. As an avid user of technology myself, I maximized my students' one to one computers to enhance learning, creativity, and technical literacy. I was excited to learn my students had one to one technology. In order to grow technical literacy SOMETHING As a teacher, I relied heavily on technology to help me maximize learning and creativity in my classroom ",
+    // "About Me \n Hi! I'm Marie Crumpton, a full-stack software developer motivated by the power of technology to create SOMETHING MORE HERE AND IN BETWEEN \n Before becoming a software developer, I worked as an elementary school teacher. As a teacher, I relied heavily on technology to help me maximize learning and creativity in my classroom. WRITE MORE \n Learn more about me by clicking on some of my favorite objects! (REPHRASE)",
   },
   ipad: {
+    name: 'Draw',
     description:
       'One of my favorite creative outlets is drawing. I especially enjoy drawing on my iPad using the Procreate app. Most of the assets for this site were drawn by me using my iPad.',
   },
   nintendoSwitch: {
+    name: 'Play',
     description:
-      "When my eyes aren't glued to VSCode or documentation, you can usually find me mapping out a new geographic feature for my island or developing my battle strategy to defeat a dragon. That's to say I really enjoy playing games like Animal Crossing, Paper Mario, and Fire Emblem on my Nintendo Switch.",
+      "When my eyes aren't glued to VSCode or documentation, you can usually find me mapping out a new geographic feature for my island or developing my battle strategy to defeat a dragon. That's to say I really enjoy playing games like Animal Crossing, Paper Mario, Spiritfarer, and Fire Emblem on my Nintendo Switch.",
   },
   teapot: {
+    name: 'Relax',
     description:
       'Keyboards are useful, styluses are great, but a warm cup of tea is the perfect accompaniment to solving bugs in my code or getting the right shapes for a drawing. My most reached for teas are chamomile, jasmine, and english breakfast.',
   },
   book: {
+    name: 'Learn',
     description:
       'When I find myself strained from staring at a screen or solving a difficult problem, I find reading books eases the tension and resets my brain. Some of my favorite reads include the Harry Potter series, The Invisible Life of Addie Larue, and Calvin & Hobbes.',
   },
